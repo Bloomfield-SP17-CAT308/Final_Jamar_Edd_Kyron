dd7a1061cf682f4fc5d5af6aa1a90a177a3ecb68
Edd Brown <edd_brown@bloomfield.edu>
2017-04-01 05:25:06 -0400

    Set up a scene to showcase how the player moves
    Directions in the game. Open up the practice scene. Still thinking about
    how I can improve it.

---
+++ Rolling2D/Assets/Roller.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Roller : MonoBehaviour 
+{
+
+    [SerializeField]
+    private float speed;
+    private float maxSpeed = 20;
+    private Rigidbody2D rb;
+    [SerializeField]
+    private  bool canJump, downATK;
+    public float jumpPower;
+    public Collider2D coll;
+    public PhysicsMaterial2D phys;
+	
+	void Start () 
+    {
+        rb = GetComponent<Rigidbody2D>();
+        speed = 5f;
+        canJump = true;
+        coll = GetComponent<Collider2D>();
+        downATK = false;
+	}
+	
+	
+	void Update () 
+    {
+        
+        Roll(1);
+        if (Input.GetKeyDown(KeyCode.Space) && canJump)
+            Jump();
+        
+
+        if (Input.GetKeyDown(KeyCode.Space) && !canJump && !downATK)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, 0);
+            rb.AddForce(new Vector2(0, -700));
+            coll.sharedMaterial = phys;
+            downATK = true;
+        }
+
+
+
+
+        if (Input.GetKeyDown(KeyCode.Q))
+            coll.sharedMaterial = null;
+
+        if (rb.velocity.x > maxSpeed)
+            rb.velocity = new Vector2(maxSpeed, rb.velocity.y);
+
+
+        if (rb.IsTouchingLayers(1<<8))
+            canJump = true;
+        else
+            canJump = false;
+
+
+	}
+
+    void Roll(float magnitude)
+    {
+        rb.AddForce(new Vector3(magnitude*speed, 0));
+    }
+
+    void Jump()
+    {
+        rb.AddForce(new Vector2(0, jumpPower));
+
+    }
+
+    void OnCollisionExit2D(Collision2D other)
+    {
+        if (downATK)
+        if (other.gameObject.CompareTag("Platform"))
+        {
+            print("what");
+            downATK = false;
+            coll.sharedMaterial = null;
+        }
+            
+            
+    }
+
+
+}
---
+++ Rolling2D/Assets/Smthnthiwnt.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Smthnthiwnt : MonoBehaviour 
+{
+
+    public GameObject post;
+	void Start () 
+    {
+        for(int i = 0; i < 40; i++)
+        {
+            Instantiate(post, new Vector3(post.transform.position.x + 10 * i, post.transform.position.y, post.transform.position.z), Quaternion.identity);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () 
+    {
+		
+	}
+
+}
